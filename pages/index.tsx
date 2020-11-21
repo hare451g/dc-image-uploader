7a942e4f8cd0944939faae78c83bbe7ebab6ee31
hendra sadewa <hendrasadewa30@gmail.com>
2020-11-21 12:16:25 +0700

    add google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import ImageUploader from '../containers/ImageUploader';
 
 import styles from '../styles/Home.module.css';
 
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Image Upload</h1>
+      <ImageUploader />
     </div>
   );
 }
