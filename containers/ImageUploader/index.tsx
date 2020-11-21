e975a8d71e7dd319239e5a254a8efde765f4383c
hendra sadewa <hendrasadewa30@gmail.com>
2020-11-21 14:28:03 +0700

    slice result screeen

--- containers/ImageUploader/index.tsx
+++ containers/ImageUploader/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ProgressScreen from './components/ProgressScreen';
+import ResultScreen from './components/ResultScreen';
 import UploadScreen from './components/UploadScreen';
 
 type PropTypes = {};
@@ -8,6 +9,7 @@ function ImageUploader(props: PropTypes) {
   // states
   const [status, setStatus] = useState<string>('idle');
   const [progress, setProgress] = useState<number>(0);
+  const [imageURL, setImageURL] = useState<string>(null);
 
   switch (status) {
     case 'idle':
@@ -15,6 +17,12 @@ function ImageUploader(props: PropTypes) {
 
     case 'pending':
       return <ProgressScreen progress={progress} />;
+
+    case 'resolved':
+      return <ResultScreen imageURL={imageURL} />;
+
+    case 'rejected':
+      return <div>error</div>;
 
     default:
       return <div>something went wrong . . . </div>;
