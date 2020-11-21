d46d2d11fdca01406289bf7137a605efe1d471bf
hendra sadewa <hendrasadewa30@gmail.com>
2020-11-21 07:30:14 +0700

    create image upload handler

--- pages/api/image-upload.js
+++
@@ -1,7 +0,0 @@
-function handler(req, res) {
-  // default response
-  res.statusCode = 403;
-  res.json({ message: 'Direct access is not allowed' });
-}
-
-export default handler;
---
+++ pages/api/image-upload.ts
@@ -0,0 +1,45 @@
+import Formidable from 'formidable';
+import {
+  NextApiHandler,
+  NextApiRequest,
+  NextApiResponse,
+  PageConfig,
+} from 'next';
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
+
+const handler: NextApiHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const form = new Formidable.IncomingForm();
+
+    form.uploadDir = './public/uploads/';
+    form.multiples = false;
+    form.keepExtensions = true;
+
+    form.parse(req, (error, fields, files) => {
+      if (error) {
+        res.status(422).json({
+          messages: 'An error occurred when parsing form',
+          error,
+        });
+      }
+
+      const { name, path: originalPath, size, type } = files.file;
+      const path = originalPath.substr('public'.length, originalPath.length);
+
+      res.status(200).json({
+        file: { size, name, type, path },
+        messages: 'Files uploaded successfully',
+      });
+    });
+  } else {
+    res.status(403).json({ message: 'Direct access is not allowed' });
+  }
+};
+
+export default handler;
